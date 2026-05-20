baa34d97b65e670737f9f58df11e9d7257e5cd60
Akash Kulbir <jesuiskashkulbir@gmail.com>
2026-05-20 18:38:27 +0200

    Register Commands

---
+++ src/registerCommands.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import { registerCommands } from "./core/discord/registerCommands.js";
+
+async function main(): Promise<void> {
+  await registerCommands();
+  console.log("[registerCommands] done");
+}
+
+main().catch((error) => {
+  console.error("[registerCommands] failed", error);
+});
